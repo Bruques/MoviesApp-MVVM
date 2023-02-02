0b32315cf5803c31f45f1aa2d3dada42c361f018
Bruno Marques <brunomarques@Brunos-MacBook-Air.local>
2023-02-01 23:26:00 -0300

    first commit

---
+++ MoviesApp-MVVM/MoviewDetailHomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesApp-MVVM
+//
+//  Created by Bruno Marques on 01/02/23.
+//
+
+import UIKit
+
+class MoviewDetailHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
